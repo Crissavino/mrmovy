6a8db7c86ef9417a96f72c3194c56c05c35c180a
Crissavino <savinocristian89@gmail.com>
2018-08-14 19:25:45 -0300

    Ahi va el commit

---
+++ js/efecto-ancla.js
@@ -0,0 +1,8 @@
+$('nav a').click(function(e){
+e.preventDefault();
+var strAncla=$(this).attr('href');
+  $('body,html').stop(true,true).animate({
+    scrollTop: $(strAncla).offset().top
+  },1000);
+
+});
